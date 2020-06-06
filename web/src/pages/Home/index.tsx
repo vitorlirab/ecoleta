00757c44a3ad7183ab953f0d28471fe30dcc2fe6
João Vitor Lira Bezerra <48421122+vitorlirab@users.noreply.github.com>
2020-06-06 11:32:33 -0300

    Update index.tsx

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => (
       <main>
         <h1>Seu marketplace de coleta de resíduos.</h1>
         <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-        <Link to="/cadastro">
+        <Link to="/create-point">
           <span>
             <FiLogIn />
           </span>
